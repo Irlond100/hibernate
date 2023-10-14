3ce6140e346e5e72e6225ab68741903cd266407f
Irlond100 <irlondito100@gmail.com>
2023-10-15 00:38:44 +0300

    Initial commit

---
+++ src/main/java/com/rakitov/hibernate/service/Service.java
@@ -0,0 +1,20 @@
+package com.rakitov.hibernate.service;
+
+import com.rakitov.hibernate.entity.Person;
+import com.rakitov.hibernate.repository.Repository;
+
+
+import java.util.List;
+
+@org.springframework.stereotype.Service
+public class Service {
+    private final Repository repository;
+
+    public Service(Repository repository) {
+        this.repository = repository;
+    }
+
+    public List<Person> getPersonsByCity(String city) {
+        return repository.getPersonsByCity(city);
+    }
+}
